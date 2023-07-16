d9d1ef448d16d3832b122aeeabfa2d91b2d81604
Lukedit <133773592+Lukedit@users.noreply.github.com>
2023-07-16 13:52:59 -0300

    Add files via upload

---
+++ ex1_js/script.js
@@ -0,0 +1,39 @@
+/*
+<aside>
+💡 O que será abordado nesse desafio:
+
+- Variáveis;
+- Operações matemáticas;
+- Operadores comparativos;
+- Condicional.
+
+</aside>
+
+Bora praticar e rever tudo o que foi ensinado na aula? **💜**
+Nesse desafio, você irá solicitar ao usuário que ele insira **dois números** e, a partir daí, calcular:
+
+- [ ]  A soma dos dois números; ok
+- [ ]  A subtração dos dois números; ok
+- [ ]  A multiplicação dos dois números; ok
+- [ ]  A divisão dos dois números; ok
+- [ ]  O resto da divisão dos dois números; ok
+
+Fácil até aqui, né? Bora se desafiar? 👀 🧑‍🚀
+
+- [ ]  Verifique se a soma dos dois números é par ~~(ou ímpar)~~;
+- [ ]  Verifique se os dois números inseridos são iguais ~~(ou diferentes)~~.
+
+Lembre-se de sempre mostrar na tela o resultado de cada uma dessas operações!
+O resultado final desse desafio deve ser algo em torno de:
+
+*/
+
+let numberOne = prompt(`insira aqui o primeiro numero`)
+let numberTwo = prompt(`insira aqui o segundo numero`)
+
+let soma = alert(`a soma dos dois numeros é: ${Number(numberOne) + Number(numberTwo)}`)
+let Sub = alert(`a subtração dos dois numeros é: ${Number(numberOne) - Number(numberTwo)}`)
+let mult = alert(`a multiplicação dos dois numeros é: ${Number(numberOne) * Number(numberTwo)}`)
+let div = alert(`a divisão dos dois numeros é: ${Number(numberOne) / Number(numberTwo)}`)
+let rest = alert(`a resto dos dois numeros é: ${Number(numberOne) % Number(numberTwo)}`)
+
